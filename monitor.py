a6099581568b097051cef5016441cdbb30c07cc7
Mahmoud Nasser <mahmoudnasser1561@gmail.com>
2025-03-03 22:48:34 +0200

    queue inspection

--- monitor.py
+++ monitor.py
@@ -34,8 +34,28 @@ def check_server_status(server):
     except requests.exceptions.RequestException as e:
         print(f"[✖] {server['name']} is down: {e}")
 
+def get_queue_depth(server):
+    try:
+        response = requests.get(f"{server['url']}/jobs", auth=server["auth"], timeout=5)
+        if response.status_code == 200:
+            jobs = response.json()
+            pending_jobs = sum(1 for job in jobs if job["Status"] == "Pending")
+            active_jobs = sum(1 for job in jobs if job["Status"] == "Running")
+
+            queue_depth = pending_jobs + active_jobs
+            print(f"queue depth: {queue_depth}, pending jobs: {pending_jobs}, active jobs: {active_jobs}")
+            return queue_depth, pending_jobs, active_jobs
+        else:
+            return "N/A", "N/A", "N/A"
+    except requests.exceptions.RequestException as e:
+        return "N/A", "N/A", "N/A"
+
+
 if __name__ == "__main__":
     for server in servers:
         check_server_status(server)
+    print("\n#--queue inspection--#")
+    for server in servers:
+        get_queue_depth(server)
     
     print(f"\nTotal DICOM Instances across all servers: {total_instances}")
